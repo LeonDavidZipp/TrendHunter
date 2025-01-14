07e0cef13b8dd27d5f33f5d11cd30cd80cea48ed
Leon David Zipp <leondavidzipp@gmx.de>
2025-01-14 21:42:25 +0100

    added tokens table and token time series tables

--- db/migrations.sql
+++ db/migrations.sql
@@ -33,34 +33,56 @@ CREATE INDEX ON "observations" ("id");
 CREATE INDEX ON "observations" ("date");
 CREATE INDEX ON "observations" ("token");
 
+CREATE TABLE IF NOT EXIST "tokens" (
+    "cmc_id" BIGSERIAL PRIMARY KEY,
+    "name" VARCHAR NOT NULL,
+    "symbol" VARCHAR NOT NULL,
+    "address" VARCHAR NOT NULL,
+    "last_checked" TIMESTAMPTZ NOT NULL DEFAULT(NOW()),
+);
+
+CREATE INDEX ON "observations" ("cmc_id");
+CREATE INDEX ON "observations" ("symbol");
+
+CREATE TABLE IF NOT EXIST "token_time_series" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "timestamp" TIMESTAMPTZ NOT NULL,
+    "price": usd_quote["price"],
+    "currency": VARCHAR NOT NULL DEFAULT("USD"),
+    "volume_24h": BIGINT,
+    "market_cap": BIGINT,
+    "circulating_supply": BIGINT,
+    "total_supply": BIGINT
+);
+
 CREATE TABLE "tweets" (
-    id BIGSERIAL PRIMARY KEY,
-    created_at TIMESTAMPTZ,
-    date TIMESTAMPTZ,
-    timezone VARCHAR,
-    tweet TEXT,
-    language VARCHAR,
-    hashtags TEXT[],
-    cashtags TEXT[],
-    user_id BIGINT,
-    user_id_str VARCHAR,
-    username VARCHAR,
-    name VARCHAR,
-    day INTEGER,
-    hour INTEGER,
-    retweet BOOLEAN,
-    nlikes INTEGER,
-    nreplies INTEGER,
-    nretweets INTEGER,
-    quote_url TEXT,
-    search TEXT,
-    near VARCHAR,
-    source VARCHAR,
-    user_rt_id BIGINT,
-    user_rt VARCHAR,
-    retweet_id BIGINT,
-    reply_to TEXT[],
-    retweet_date TIMESTAMPTZ,
+    "id" BIGSERIAL PRIMARY KEY,
+    "created_at" TIMESTAMPTZ,
+    "date" TIMESTAMPTZ,
+    "timezone" VARCHAR,
+    "tweet" TEXT,
+    "language" VARCHAR,
+    "hashtags" TEXT[],
+    "cashtags" TEXT[],
+    "user_id" BIGINT,
+    "user_id_str" VARCHAR,
+    "username" VARCHAR,
+    "name" VARCHAR,
+    "day" INTEGER,
+    "hour" INTEGER,
+    "retweet" BOOLEAN,
+    "nlikes" INTEGER,
+    "nreplies" INTEGER,
+    "nretweets" INTEGER,
+    "quote_url" TEXT,
+    "search" TEXT,
+    "near" VARCHAR,
+    "source" VARCHAR,
+    "user_rt_id" BIGINT,
+    "user_rt" VARCHAR,
+    "retweet_id" BIGINT,
+    "reply_to" TEXT[],
+    "retweet_date" TIMESTAMPTZ,
 );
 
 CREATE INDEX ON "tweets" ("date");
